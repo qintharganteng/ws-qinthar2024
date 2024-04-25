7a59dbac3996d9210fc480f30e546cbf853763e0
qintharganteng <qintharalmaliki@gmail.com>
2024-04-25 12:05:30 +0700

    heroku

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/qintharganteng/ws-qinthar2024/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 
-	"iteung/url"
+	"github.com/qintharganteng/ws-qinthar2024/url"
 
 	"github.com/gofiber/fiber/v2"
 )
