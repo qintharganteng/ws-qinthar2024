cc349c65919fe3bda6b19c900c3fc06e2a46ae69
qintharganteng <qintharalmaliki@gmail.com>
2024-04-26 14:17:28 +0700

    updatep6

--- url/url.go
+++ url/url.go
@@ -2,7 +2,6 @@ package url
 
 import (
 	"github.com/qintharganteng/ws-qinthar2024/controller"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -17,4 +16,7 @@ func Web(page *fiber.App) {
 	page.Delete("/", controller.Sink)
 	page.Options("/", controller.Sink)
 
+	page.Get("/checkip", controller.Homepage) //ujicoba panggil package musik
+	page.Get("/presensi", controller.GetPresensi)
+	page.Get("/presensi/:id", controller.GetPresensiID) //menampilkan data presensi berdasarkan id
 }
