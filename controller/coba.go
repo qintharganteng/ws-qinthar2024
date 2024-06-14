e8462b5f4f9d1360646ada593512d481c0dc3893
qintharganteng <qintharalmaliki@gmail.com>
2024-06-14 15:51:44 +0700

    putswaggger

--- controller/coba.go
+++ controller/coba.go
@@ -121,6 +121,18 @@ func InsertDataPresensi(c *fiber.Ctx) error {
 	})
 }
 
+// UpdateData godoc
+// @Summary Update data presensi.
+// @Description Ubah data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param id path string true "Masukan ID"
+// @Param request body ReqPresensi true "Payload Body [RAW]"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 500
+// @Router /update/{id} [put]
 func UpdateData(c *fiber.Ctx) error {
 	db := config.Ulbimongoconn
 
